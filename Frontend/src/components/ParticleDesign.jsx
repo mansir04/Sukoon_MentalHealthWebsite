9aa153cb4fec6e5a8b3bf836075e5e9f8c61ad44
mansir04 <121948576+mansir04@users.noreply.github.com>
2023-08-27 13:52:51 +0530

    Added Animations and Improved  home page UI
    Added the particles.js animation. Used Framer motion for sliding in animation. Both in the home page. Fixed alignments. Added a logo in navbar and some little animations for hover effect.

---
+++ Frontend/src/components/ParticleDesign.jsx
@@ -0,0 +1,101 @@
+import React, { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+// Import the CSS file
+
+const ParticleAnimation = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <div className="particles-container">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fullScreen: false,
+          background: {
+            color: {
+              value: "#ffffff", // Red background color
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+                distance: 400,
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 150,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#000000", // White particles color
+            },
+            links: {
+              color: "#000000", // White links color
+              distance: 120,
+              enable: true,
+              opacity: 0.6,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1200,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 2 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default ParticleAnimation;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Logoslider from '../components/Logoslider';
 import Home_info from '../components/Home_info';
+import Design from "../components/ParticleDesign";
 
 const Home = () => {
   const [joke, setJoke] = useState('');
@@ -19,26 +20,27 @@ const Home = () => {
     }
   };
 
-  return (
-    <>
-    <h1>A One Stop Solution For All Your Mental Health Needs !</h1>
-    {'\n'}
-    {'\n'}
+    return (
+      <div className='home-page'>
+        <Design />
+        <div className="helpline-container">
+      <h2>Helpline Numbers  :  1800-599-0019   102</h2>
 
-    <div className="random-joke">
-      <header className="random-joke-header">
-      <blockquote>
-        <div className='joke-text'>
-        <p>Start Your Day With Us and a Smile 😊 </p>
-        </div>
-        <p>{joke}</p>
-        </blockquote>
-      </header>
     </div>
-    <Home_info/>
-    <Logoslider/>
-    </>
-  );
-};
-
+        <h1>A One Stop Solution For All Your Mental Health Needs !</h1>
+        <div className="random-joke">
+          <header className="random-joke-header">
+            <blockquote>
+              <div className='joke-text'>
+                <p>Start Your Day With Us and a Smile 😊 </p>
+              </div>
+              <p>{joke}</p>
+            </blockquote>
+          </header>
+        </div>
+        <Home_info />
+        <Logoslider />
+      </div>
+    );
+  }; 
 export default Home;
